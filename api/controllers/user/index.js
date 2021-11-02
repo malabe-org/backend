1e571c7cb6c7634eda4f4e55a1945733bd01afcb
klemakle <kalidou1309@gmail.com>
2021-11-02 01:10:58 +0000

    one change

--- api/controllers/user/index.js
+++ api/controllers/user/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userController = require("./user.controller");
 const auth = require("../../middlewares/auth")
 
+router.get("", auth.isAuth, auth.isAdmin, userController.getAllUsers);
 router.post('/logout', auth.isAuth, userController.logout);
 router.post('/login', userController.login);
 router.post('/signup', userController.signup);
-router.get("/users", auth.isAuth, auth.isAdmin, userController.getAllUsers);
-router.get('/users/:role', auth.isAuth, auth.isAdmin, userController.getSpecificUsers);
+router.get('/role/:role', auth.isAuth, auth.isAdmin, userController.getSpecificUsers);
 
 module.exports = router;
